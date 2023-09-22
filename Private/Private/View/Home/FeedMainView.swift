a173e15d2500ba0ac1d4d2f09f9934c517643203
Sangwoo Byeon <tkddn0518@naver.com>
2023-09-22 15:43:39 +0900

    [Create] 피드뷰, 지도뷰 생성

---
+++ Private/Private/View/Home/FeedMainView.swift
@@ -0,0 +1,20 @@
+//
+//  FeedMainView.swift
+//  Private
+//
+//  Created by 변상우 on 2023/09/22.
+//
+
+import SwiftUI
+
+struct FeedMainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FeedMainView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedMainView()
+    }
+}
