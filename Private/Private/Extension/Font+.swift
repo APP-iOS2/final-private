cc07aba14cc609bc960145527d8454ef3268f748
Sangwoo Byeon <tkddn0518@naver.com>
2023-09-22 09:24:35 +0900

    [Add] 폰트 추가

---
+++ Private/Private/Extension/Font+.swift
@@ -0,0 +1,29 @@
+//
+//  Font+.swift
+//  Private
+//
+//  Created by 변상우 on 2023/09/21.
+//
+
+import SwiftUI
+
+extension Font {
+    
+    // Bold
+    static let pretendardBold28: Font = .custom("Pretendard-Bold", size: 28)
+    static let pretendardBold24: Font = .custom("Pretendard-Bold", size: 24)
+    static let pretendardBold18: Font = .custom("Pretendard-Bold", size: 18)
+    static let pretendardBold14: Font = .custom("Pretendard-Bold", size: 14)
+    
+    // SemiBold
+    static let pretendardSemiBold16: Font = .custom("Pretendard-SemiBold", size: 16)
+    
+    // Medium
+    static let pretendardMedium18: Font = .custom("Pretendard-Medium", size: 18)
+    static let pretendardMedium16: Font = .custom("Pretendard-Medium", size: 16)
+    
+    // Regular
+    static let pretendardRegular14: Font = .custom("Pretendard-Regular", size: 14)
+    static let pretendardRegular16: Font = .custom("Pretendard-Regular", size: 16)
+    
+}
