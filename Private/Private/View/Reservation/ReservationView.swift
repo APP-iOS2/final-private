2368e2cf593adc81d50645ee1adfccb20bb51009
Sunghun Park <dfstem9@gmail.com>
2023-09-22 14:50:12 +0900

    [Add] ItemInfoView 파일 및 관련 UI 추가

--- Private/Private/View/Reservation/ReservationView.swift
+++ Private/Private/View/Reservation/ReservationView.swift
@@ -15,7 +15,12 @@ struct ReservationView: View {
     @Binding var selection: Int
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            ItemInfoView(shopStore: shopStore)
+        }
+        .padding()
+        
     }
 }
 
