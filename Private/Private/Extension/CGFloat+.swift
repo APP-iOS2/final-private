cfae78783cc32078081d8016d57ec76e5199a3be
Sangwoo Byeon <tkddn0518@naver.com>
2023-09-24 17:26:53 +0900

    [Create] CGFloat extension 파일 추가

---
+++ Private/Private/Extension/CGFloat+.swift
@@ -0,0 +1,13 @@
+//
+//  CGFloat+.swift
+//  Private
+//
+//  Created by 변상우 on 2023/09/23.
+//
+
+import SwiftUI
+
+extension CGFloat {
+    static let screenWidth = UIScreen.main.bounds.width
+    static let screenHeight = UIScreen.main.bounds.height
+}
