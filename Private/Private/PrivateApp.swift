5094598b2badbe9cdd336ea98d685a2b87d2af48
Sangwoo Byeon <tkddn0518@naver.com>
2023-09-25 16:23:32 +0900

    [Add] ChatRoomStore envirionmentObject 추가

--- Private/Private/PrivateApp.swift
+++ Private/Private/PrivateApp.swift
@@ -15,6 +15,7 @@ struct PrivateApp: App {
             LaunchView()
                 .environmentObject(FeedStore())
                 .environmentObject(SearchStore())
+                .environmentObject(ChatRoomStore())
         }
     }
 }
