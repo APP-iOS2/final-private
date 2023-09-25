b66bf6e4fadd154da334069ce98d598df4600c4e
dev-yeon <110480577+dev-yeon@users.noreply.github.com>
2023-09-26 03:22:27 +0900

    [Move] [Add]
     MapReviewSheetView  파일에서 뷰를 분리 MapReviewRowView로 만들어 코드 재사용성을 높였습니다.
    MapReviewEmptyView 를 생성하여, 내가 팔로잉한 사람의 해당 가게 리뷰가 아무도 없는 상황에서 뜰 화면을 추가해보았습니다  (+ 팔로잉 하는 사람이 아무도 없을때와, 회원가입을 하지 않은 화면에도 문구를 달리해서 띄워볼까 합니다.
    ..  기왕이면 통일시키는게 좋아서 날 밝고 이야기하는대로 작업진행해보겠습니다.

---
+++ Private/Private/View/Map/MapMarkerDetail/MapReviewEmptyView.swift
@@ -0,0 +1,20 @@
+//
+//  MapReviewEmptyView.swift
+//  Private
+//
+//  Created by yeon I on 2023/09/26.
+//
+
+import SwiftUI
+
+struct MapReviewEmptyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapReviewEmptyView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapReviewEmptyView()
+    }
+}
---
+++ Private/Private/View/Map/MapMarkerDetail/MapReviewRowView.swift
@@ -0,0 +1,20 @@
+//
+//  MapReviewRowView.swift
+//  Private
+//
+//  Created by yeon I on 2023/09/26.
+//
+
+import SwiftUI
+
+struct MapReviewRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapReviewRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapReviewRowView()
+    }
+}
