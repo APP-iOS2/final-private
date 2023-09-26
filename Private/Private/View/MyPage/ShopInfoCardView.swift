8975082b63f6c836a7b813606995cdbe53fd5f1f
jin_hyoung <37445660+mhn03148@users.noreply.github.com>
2023-09-26 11:52:26 +0900

    [Feat] InfoCardView 작성

--- Private/Private/View/MyPage/ShopInfoCardView.swift
+++ Private/Private/View/MyPage/ShopInfoCardView.swift
@@ -6,13 +6,71 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct ShopInfoCardView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            KFImage(URL(string:UserStore.user.bookmark[0].shopImageURL)) .placeholder {
+                Image(systemName: "photo")
+            }
+            .resizable()
+            .frame(width: 80,height: 80)
+            .cornerRadius(10)
+            .padding(.leading,20)
+            VStack(alignment: .leading) {
+                Text(UserStore.user.bookmark[0].name)
+                    .font(.pretendardBold18)
+                    .padding(.bottom, 2)
+                VStack(alignment: .leading) {
+                    HStack {
+                        Label(
+                            title: { Text(UserStore.user.bookmark[0].category.categoryName)
+                                    .font(.pretendardRegular14)
+                            },
+                            icon: { Image(systemName: "fork.knife") }
+                        )
+                        Text("|")
+                            .font(.pretendardRegular14)
+                        Label(
+                            title: { Text("0") // 가게 좋아요 수 필요
+                                    .font(.pretendardRegular14)
+                            },
+                            icon: { Image(systemName: "heart.fill") }
+                        )
+                            .font(.pretendardRegular14)
+                        Text("|")
+                            .font(.pretendardRegular14)
+                        Label(
+                            title: { Text("0") // 가게 북마크 수 필요
+                                    .font(.pretendardRegular14)
+                            },
+                            icon: { Image(systemName: "bookmark.fill") }
+                        )
+                            .font(.pretendardRegular14)
+                    }
+                    Label(
+                        title: { Text(UserStore.user.bookmark[0].address)
+                                .font(.pretendardRegular14)
+                        },
+                        icon: { Image(systemName: "mappin") }
+                    )
+                }
+                .padding(.top,2)
+            }
+            .padding(.leading,5)
+            Spacer()
+            Image(systemName: "pin.circle.fill")
+                .resizable()
+                .frame(width: 30, height: 30)
+                .foregroundColor(.accentColor)
+                .padding(.trailing,20)
+        }
     }
 }
 
-#Preview {
-    ShopInfoCardView()
+struct ShopInfoCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShopInfoCardView()
+    }
 }
