133e56ad76f1551b874d429653510d2ff0ab5070
Segeun Choi <chltprms77@icloud.com>
2023-09-26 17:28:22 +0900

    [Feat] PictureView추가, category 3개까지 선택가능
    PictureView추가,
    category 3개까지 선택가능
    초과 시 경고 Alert
    PostView 수정
    FeedStore에 파베관련 메서드 추가
    파베연동은 내일부터 시작

--- Private/Private/View/Post/CatecoryView.swift
+++ Private/Private/View/Post/CatecoryView.swift
@@ -10,6 +10,11 @@ import SwiftUI
 struct CatecoryView: View {
     
     @State private var selectedCategory: Category = .koreanFood
+    @State private var selectedCategories: Set<Category> = []
+    @State private var showAlert = false
+
+    private let maxSelectedCategories = 3
+    
     private let gridItems: [GridItem] = [
         GridItem(.adaptive(minimum: 70))
         //        .init(.flexible(), spacing: 10),
@@ -18,13 +23,14 @@ struct CatecoryView: View {
         //        .init(.flexible(), spacing: 10),
         //        .init(.flexible(), spacing: 10)
     ]
+    
     var body: some View {
         VStack(alignment: .leading) {
             
             HStack {
                 Text("카테고리")
                     .font(.title2).fontWeight(.semibold)
-                Text("(최대3개)")
+                Text("(최대 3개)")
                     .font(.footnote)
                     .foregroundColor(.secondary)
             }
@@ -32,37 +38,56 @@ struct CatecoryView: View {
             LazyVGrid(columns: gridItems) {
                 ForEach (Category.allCases, id: \.rawValue) { category in
                     VStack {
-                        if category == selectedCategory {
+                        if selectedCategories.contains(category) {
                             Text(category.categoryName)
                                 .font(.body)
                                 .foregroundColor(.black)
-                                .padding(.vertical,4)
+                                .padding(.vertical, 4)
                                 .padding(.horizontal, 4)
                                 .background(Color.accentColor)
                                 .cornerRadius(7)
-                            
-                            
                         } else {
                             Text(category.categoryName)
-                                .padding(.vertical,4)
+                                .padding(.vertical, 4)
                                 .padding(.horizontal, 4)
                                 .cornerRadius(7)
                                 .overlay(
                                     RoundedRectangle(cornerRadius: 7)
                                         .stroke(Color.darkGrayColor, lineWidth: 1.5)
-                                    //                                        .frame(width: 70, height: 28)
                                 )
                         }
                     }
                     .onTapGesture {
-                        self.selectedCategory = category
-                        print(self.selectedCategory)
+                        if selectedCategories.count < maxSelectedCategories || selectedCategories.contains(category) {
+                            toggleCategorySelection(category)
+                            print(selectedCategories)
+                        } else {
+                            showAlert = true
+                            print("3개 초과 선택")
+                        }
                     }
+                    .alert(isPresented: $showAlert) {
+                        Alert(
+                            title: Text("선택 초과"),
+                            message: Text("최대 3개까지 선택 가능합니다."),
+                            dismissButton: .default(Text("확인"))
+                        )
+                    }
+
                 }
             }
         }
     }
+    
+    private func toggleCategorySelection(_ category: Category) {
+        if selectedCategories.contains(category) {
+            selectedCategories.remove(category)
+        } else {
+            selectedCategories.insert(category)
+        }
+    }
 }
+
 
 
 struct CatecoryView_Previews: PreviewProvider {
