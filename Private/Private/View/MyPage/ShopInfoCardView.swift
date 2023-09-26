56d06b9045e75772f2396878d53dbb20e1751f7d
jin_hyoung <37445660+mhn03148@users.noreply.github.com>
2023-09-26 10:44:02 +0900

    [Create] 내가 저장한 가게 정보 간단히 보여주는 카드 뷰 생성

---
+++ Private/Private/View/MyPage/ShopInfoCardView.swift
@@ -0,0 +1,18 @@
+//
+//  ShopInfoCardView.swift
+//  Private
+//
+//  Created by 주진형 on 2023/09/26.
+//
+
+import SwiftUI
+
+struct ShopInfoCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShopInfoCardView()
+}
