0af0b59c18702b06faa7780e9ad43554f4db0cb3
jin_hyoung <37445660+mhn03148@users.noreply.github.com>
2023-09-27 15:28:05 +0900

    [Fix] SettingView(로그아웃 추가), ShopInfoCardView 재 작성

--- Private/Private/View/MyPage/SettingView.swift
+++ Private/Private/View/MyPage/SettingView.swift
@@ -2,17 +2,87 @@
 //  SettingView.swift
 //  Private
 //
-//  Created by 주진형 on 2023/09/27.
+//  Created by 주진형 on 2023/09/26.
 //
 
 import SwiftUI
 
 struct SettingView: View {
+    @EnvironmentObject var authStore: AuthStore
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    var backButton : some View {
+        Button {
+            self.presentationMode.wrappedValue.dismiss()
+        } label: {
+            Image(systemName: "chevron.left")
+                .foregroundColor(.primary)
+        }
+    } // custom backbutton
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List {
+                Section (content: {
+                    Toggle(isOn: .constant(true), label: {
+                        Text("모든알림")
+                            .font(.pretendardBold18)
+                    })
+                }, header: {
+                    Text("알림")
+                        .font(.pretendardRegular12)
+                })
+                Section (content: {
+                    HStack {
+                        VStack {
+                            Text("v1.0.0")
+                                .font(.pretendardBold18)
+                        }
+                        Spacer()
+                        Text("최신 버전입니다.")
+                            .font(.pretendardRegular12)
+                    }
+                    
+                }, header: {
+                    Text("현재버전")
+                        .font(.pretendardRegular12)
+                })
+                Section (content: {
+                    Button{
+                        authStore.signOutGoogle()
+                    } label: {
+                        HStack {
+                            Text("로그아웃")
+                            Spacer()
+                            Image(systemName: "chevron.right")
+                        }
+                    }
+                    .foregroundColor(.primary)
+                    
+                    Button{
+                        print("계정삭제")
+                    } label: {
+                        HStack {
+                            Text("계정삭제")
+                            Spacer()
+                            Image(systemName: "chevron.right")
+                        }
+                    }
+                    .foregroundColor(.red)
+                    
+                }, header: {
+                    Text("계정관리")
+                        .font(.pretendardRegular12)
+                })
+            }
+            .navigationTitle("설정")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarBackButtonHidden(true)
+            .navigationBarItems(leading: backButton)
+        }
     }
 }
 
-#Preview {
-    SettingView()
+struct SettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingView()
+    }
 }
