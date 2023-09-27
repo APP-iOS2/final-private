b817879dd81e592353f98c6182f8a752f4f510d3
jin_hyoung <37445660+mhn03148@users.noreply.github.com>
2023-09-27 13:03:57 +0900

    [Fix] bservedObject var userStore: UserStore 추가

--- Private/Private/View/MyPage/MySavedView.swift
+++ Private/Private/View/MyPage/MySavedView.swift
@@ -8,11 +8,12 @@
 import SwiftUI
 
 struct MySavedView: View {
+    @ObservedObject var userStore: UserStore
     var columns: [GridItem] = [GridItem(.fixed(.screenWidth*0.95*0.3), spacing: 1, alignment:  nil),
                                GridItem(.fixed(.screenWidth*0.95*0.3), spacing: 1, alignment:  nil),
                                GridItem(.fixed(.screenWidth*0.95*0.3), spacing: 1, alignment:  nil)]
     var body: some View {
-        if UserStore.user.savedFeed.isEmpty {
+        if userStore.user.savedFeed.isEmpty {
             Text("저장한 피드가 없습니다")
                 .font(.pretendardBold24)
                 .padding()
@@ -23,7 +24,7 @@ struct MySavedView: View {
                     alignment: .center,
                     spacing: 1
                 ) {
-                    ForEach(UserStore.user.savedFeed, id: \.self) { feed in
+                    ForEach(userStore.user.savedFeed, id: \.self) { feed in
                         AsyncImage(url:URL(string:feed.images[0])) { image in
                             image
                                 .resizable()
@@ -41,6 +42,6 @@ struct MySavedView: View {
 
 struct MySavedView_Previews: PreviewProvider {
     static var previews: some View {
-        MySavedView()
+        MySavedView(userStore: UserStore())
     }
 }
