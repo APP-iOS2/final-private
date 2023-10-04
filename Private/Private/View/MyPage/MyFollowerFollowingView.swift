bbef7bae3b62a14e234801479f72c60b8a161e21
jin_hyoung <37445660+mhn03148@users.noreply.github.com>
2023-10-04 16:28:46 +0900

    [Feat] MypageView에서 팔로우, 팔로잉을 누르면 MyFollowerFollowingview로 넘어감, 해담 뷰에서 나의 팔로우 목록과 팔로잉 목록을 볼 수 있음

--- Private/Private/View/MyPage/MyFollowerFollowingView.swift
+++ Private/Private/View/MyPage/MyFollowerFollowingView.swift
@@ -8,11 +8,54 @@
 import SwiftUI
 
 struct MyFollowerFollowingView: View {
+    @EnvironmentObject private var userStore: UserStore
+    @State var viewNumber: Int
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            HStack {
+                Spacer()
+                Button {
+                    viewNumber = 0
+                } label: {
+                    HStack {
+                        Text("\(userStore.user.follower.count)")
+                        Text("팔로워")
+                    }
+                    .padding(.bottom, 15)
+                    .foregroundColor(.primary)
+                    .modifier(BottomBorder(showBorder: viewNumber == 0))
+                }
+                Spacer()
+                Button {
+                    viewNumber = 1
+                } label: {
+                    HStack {
+                        Text("\(userStore.user.following.count)")
+                        Text("팔로잉")
+                    }
+                    .padding(.bottom, 15)
+                    .foregroundColor(.primary)
+                    .modifier(BottomBorder(showBorder: viewNumber == 1))
+                }
+                Spacer()
+            }
+            .padding(.bottom, 10)
+            switch viewNumber {
+            case 0:
+                MyFollowerView()
+            case 1:
+                MyFollowingView()
+            default:
+                MyFollowerView()
+            }
+        }
+        .navigationTitle("\(userStore.user.nickname)")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
-#Preview {
-    MyFollowerFollowingView()
+struct MyFollowerFollowingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyFollowerFollowingView(viewNumber: 0).environmentObject(UserStore())
+    }
 }
