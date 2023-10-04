efa2ce2f06de6fde2f74a35f34b0a1607579c909
jin_hyoung <37445660+mhn03148@users.noreply.github.com>
2023-10-04 16:29:25 +0900

    [Feat] 팔로잉 뷰를 보여주는 목록

--- Private/Private/View/MyPage/MyFollowingView.swift
+++ Private/Private/View/MyPage/MyFollowingView.swift
@@ -8,11 +8,37 @@
 import SwiftUI
 
 struct MyFollowingView: View {
+    @EnvironmentObject var userStore: UserStore
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            ForEach(userStore.user.following, id:\.self) { following in
+                HStack {
+                    Text("\(following)")
+                        .foregroundColor(.primary)
+                    Spacer()
+                    Button {
+                        
+                    } label: {
+                        Text("팔로우")
+                            .padding(EdgeInsets(top: 5, leading: 10, bottom: 5, trailing: 10))
+                            .font(.pretendardBold14)
+                            .foregroundColor(.black)
+                            .background(Color.primary)
+                            .cornerRadius(20)
+                        
+                    }
+                }
+                .padding(EdgeInsets(top: 10, leading: 25, bottom: 10, trailing: 25))
+                Divider()
+                    .background(Color.primary)
+                    .frame(width: .screenWidth*0.9)
+            }
+        }
     }
 }
 
-#Preview {
-    MyFollowingView()
+struct MyFollowingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyFollowingView().environmentObject(UserStore())
+    }
 }
