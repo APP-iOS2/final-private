98970f079c649dc80909ad5ab89077c952998db8
Sunghun Park <dfstem9@gmail.com>
2023-10-05 11:41:32 +0900

    [Fix] 내 예약목록 파베 다시 연결

--- Private/Private/View/Reservation/Reservation/MyReservation.swift
+++ Private/Private/View/Reservation/Reservation/MyReservation.swift
@@ -11,33 +11,14 @@ struct MyReservation: View {
     @EnvironmentObject var reservationStore: ReservationStore
     @Binding var isShowingMyReservation: Bool
     
-//     var body: some View {
-//         ScrollView {
-//             ForEach(reservationStore.reservationList, id: \.self) { reservation in
-//                 ReservationCardView(reservation: reservation)
-//             }
-//             .padding()
-//         }
-//        .onAppear {
-//            dump(reservationStore.reservationList)
-//        }
-    let dummyReservationList: [Reservation] = [
-        Reservation(shopId: "1234-1234-1234", reservedUserId: "5678-5678-5678", date: Date(), time: 10, totalPrice: 100000),
-        Reservation(shopId: "1234-1234-1234", reservedUserId: "5678-5678-5678", date: Date(), time: 10, totalPrice: 100000),
-        Reservation(shopId: "1234-1234-1234", reservedUserId: "5678-5678-5678", date: Date(), time: 10, totalPrice: 100000),
-        Reservation(shopId: "1234-1234-1234", reservedUserId: "5678-5678-5678", date: Date(), time: 10, totalPrice: 100000),
-        Reservation(shopId: "1234-1234-1234", reservedUserId: "5678-5678-5678", date: Date(), time: 10, totalPrice: 100000),
-    ]
-    
     var body: some View {
         NavigationStack {
             ScrollView(.vertical, showsIndicators: false) {
                 LazyVStack {
-                    //                ForEach(reservationStore.reservationList, id: \.self) { reservation in
-                    ForEach(dummyReservationList, id: \.self) { reservation in
+                    ForEach(reservationStore.reservationList, id: \.self) { reservation in
                         ReservationCardView(reservation: reservation)
-                            .padding(.vertical, 6)
                     }
+                    .padding()
                 }
                 .padding()
                 .toolbar {
@@ -52,12 +33,6 @@ struct MyReservation: View {
                 }
             }
         }
-        //            .onAppear {
-        //                dump(reservationStore.reservationList)
-        //            }
-        //            .refreshable {
-        //                reservationStore.fetchReservation()
-        //            }
     }
 }
 
