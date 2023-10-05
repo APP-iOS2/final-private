36100f66585b1a463c9267d28a43a0c15cf78387
Sangwoo Byeon <tkddn0518@naver.com>
2023-10-05 10:50:03 +0900

    [Design] 로그인 화면 디자인 구성

--- Private/Private/View/MainLoginView.swift
+++ Private/Private/View/MainLoginView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import Firebase
+import GoogleSignInSwift
 
 struct MainLoginView: View {
     
@@ -15,11 +16,30 @@ struct MainLoginView: View {
     
     var body: some View {
         VStack {
-            Button {
-                authStore.signInGoogle()
-            } label: {
-                Text("구글 로그인")
+            Spacer()
+            Text("Private.")
+                .font(.pretendardBold28)
+                .foregroundStyle(.primary)
+            
+            Spacer()
+            HStack {
+                ZStack {
+                    Divider()
+                        .foregroundStyle(.primary)
+                    Text("간편 로그인")
+                        .font(.pretendardBold14)
+                        .foregroundStyle(.primary)
+                        .padding()
+                        .background(Color.tabColor)
+                }
             }
+            .padding()
+            
+            GoogleSignInButton {
+                authStore.signInGoogle()
+            }
+            .frame(width: .screenWidth * 0.9)
+            Spacer()
         }
         .onDisappear {
             userStore.fetchCurrentUser(userEmail: (authStore.currentUser?.email)!)
