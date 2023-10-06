25f474872ba09b10aaa5742860b0a080a4958ff8
jin_hyoung <37445660+mhn03148@users.noreply.github.com>
2023-10-06 15:23:37 +0900

    [Feat] 프로필 수정 뷰에서 유저 프로필 사진 업로드, 교체 가능한 코드 작성

--- Private/Private/View/MyPage/UserInfoModifyView.swift
+++ Private/Private/View/MyPage/UserInfoModifyView.swift
@@ -7,31 +7,69 @@
 
 import SwiftUI
 import Kingfisher
+import FirebaseStorage
 
 struct UserInfoModifyView: View {
     @EnvironmentObject private var userStore: UserStore
     @Binding var isModify: Bool
     @State var mypageNickname: String
+    @State var isImagePickerPresented: Bool = false
+    @State private var selectedImage: UIImage?
+    var storage = Storage.storage()
     var body: some View {
         NavigationStack {
             ScrollView {
-                ZStack {
-                    if userStore.user.profileImageURL.isEmpty {
-                        Circle()
-                            .frame(width: .screenWidth*0.23)
-                        Image(systemName: "person.fill")
-                            .resizable()
-                            .frame(width: .screenWidth*0.23,height: 80)
-                            .foregroundColor(.gray)
-                            .clipShape(Circle())
-                        Image(systemName: "photo.fill")
-                            .resizable()
-                            .frame(width: 50, height: 50)
-                            .foregroundColor(.black)
-                    } else {
-                        KFImage(URL(string: userStore.user.profileImageURL))
-                            .frame(width: .screenWidth*0.23)
+                Button {
+                    isImagePickerPresented.toggle()
+                } label: {
+                    ZStack {
+                        if userStore.user.profileImageURL.isEmpty {
+                            Circle()
+                                .frame(width: .screenWidth*0.23)
+                                .foregroundColor(.primary)
+                            if ((selectedImage) != nil) {
+                                Image(uiImage: selectedImage!)
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: .screenWidth*0.25, height: .screenWidth*0.25)
+                                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                            } else {
+                                Image(systemName: "person.fill")
+                                    .resizable()
+                                    .frame(width: .screenWidth*0.23,height: 80)
+                                    .foregroundColor(.gray)
+                                    .clipShape(Circle())
+                            }
+                            
+                        } else {
+                            if ((selectedImage) != nil) {
+                                Image(uiImage: selectedImage!)
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: .screenWidth*0.25, height: .screenWidth*0.25)
+                                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                            } else {
+                                KFImage(URL(string: userStore.user.profileImageURL))
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: .screenWidth*0.25, height: .screenWidth*0.25)
+                                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                            }
+                        }
+                        ZStack {
+                            Circle()
+                                .frame(width: 35, height: 35)
+                                .foregroundColor(Color("AccentColor"))
+                            Image(systemName: "camera")
+                                .resizable()
+                                .frame(width: 25, height: 20)
+                                .foregroundColor(.black)
+                        }
+                        .padding([.top, .leading], 55)
                     }
+                }
+                .sheet(isPresented: $isImagePickerPresented) {
+                    UserImagePickerView(selectedImage: $selectedImage)
                 }
                 Divider()
                     .background(Color.primary)
@@ -65,13 +103,41 @@ struct UserInfoModifyView: View {
             }
             ToolbarItem(placement: .topBarTrailing) {
                 Button {
-                    userStore.user.nickname = mypageNickname
+                    if mypageNickname != "" {
+                        userStore.user.nickname = mypageNickname
+                    }
+                    if selectedImage != nil {
+                        uploadimage(img: selectedImage!)
+                    }
                     userStore.updateUser(user: userStore.user)
                     isModify = false
                 } label: {
                     Text("수정")
                         .font(.pretendardBold18)
                         .foregroundColor(.accentColor)
+                }
+            }
+        }
+    }
+    func uploadimage(img: UIImage) {
+        let imageData = img.jpegData(compressionQuality: 0.2)!
+        let metaData = StorageMetadata()
+        metaData.contentType = "image/jpeg"
+        let storageRef = storage.reference().child(UUID().uuidString + ".jpg")
+        storageRef.putData(imageData,metadata: metaData) {
+            (metaData,error) in if let error = error {
+                print(error.localizedDescription)
+                return
+            } else {
+                print("사진 올리기 성공")
+            }
+            storageRef.downloadURL { url, error in
+                if let error = error {
+                    print("이미지 URL 가져오기 실패: \(error.localizedDescription)")
+                }
+                if let imageUrl = url {
+                    userStore.user.profileImageURL = imageUrl.absoluteString
+                    userStore.updateUser(user: userStore.user)
                 }
             }
         }
