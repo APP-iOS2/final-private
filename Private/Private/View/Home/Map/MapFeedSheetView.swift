f08b322df7b98e0deef0fbc027045a8adc99413f
Sunghun Park <dfstem9@gmail.com>
2023-10-06 15:03:43 +0900

    [Fix] MapFeed 오휴 해결

--- Private/Private/View/Home/Map/MapFeedSheetView.swift
+++ Private/Private/View/Home/Map/MapFeedSheetView.swift
@@ -12,7 +12,7 @@ struct MapFeedSheetView: View {
     
     var body: some View {
         List(feedStore.feedList) { feed in
-            MapMarkerFeedView(feed: feed)
+            MapMarkerFeedView()
         }
         .listStyle(.plain)
         .clipShape(RoundedRectangle(cornerRadius: 20))
