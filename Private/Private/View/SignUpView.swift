8e050402e8a04dc097b937242f76d0fcaaf9d95c
Segeun Choi <chltprms77@icloud.com>
2023-10-12 00:14:50 +0900

    [Feat] 닉네임 중복체크, 전화번호 Count제한 등 기능구현
    닉네임 중복체크(영어, 한글, 숫자만 가능)
    특수문자 포함 시
    형식에 맞지않는다는 주의 알림 추가
    전화번호 11자리로 제한(그 이상은 입력값이 아예 불가하게 구현)

--- Private/Private/View/SignUpView.swift
+++ Private/Private/View/SignUpView.swift
@@ -7,50 +7,129 @@
 
 import SwiftUI
 
+enum Field {
+    case nickName
+    
+}
 struct SignUpView: View {
     
     @EnvironmentObject var authStore: AuthStore
     @EnvironmentObject var userStore: UserStore
-    
+    @State private var checkNicknameColor: Color = Color.red
+
     @State private var nickName: String = ""
     @State private var phoneNumber: String = ""
+    @State private var cautionNickname: String = ""
     
+    @State private var isHiddenCheckButton: Bool = false
+    @State private var checkNickname: Bool = false /// 닉네임 중복 확인 Bool 값
+    @State private var isNicknameValid: Bool = true
+    
+    @FocusState private var focusField: Field?
+    
+    private let phoneNumberMaximumCount: Int = 11  /// 휴대폰 번호 최대 글자수
+
+//    var isNextAvailable: Bool {
+//        switch focusField {
+//        case .nickName:
+//            return !nickName.isEmpty && isValidNickname(nickName)
+//        case .none:
+//            return false
+//        }
+//    }
+//    var isFieldAllWrite: Bool {
+//        return !nickName.isEmpty &&
+//        checkNickname == true
+//    }
+//    텍스트 관련 연산프로퍼티 보류
     var body: some View {
         VStack {
             Spacer()
-            
             Text("Private")
                 .font(.pretendardBold28)
                 .foregroundStyle(.primary)
-            
             Spacer()
-            
-            TextField("닉네임을 입력하세요", text: $nickName) // 이 부분에 중복확인
-                .textInputAutocapitalization(.never) // 첫글자 대문자 비활성화
-                .disableAutocorrection(true) // 자동수정 비활성화
-                .padding(10)
-                .padding(.leading, 5)
-                .background(Color.lightGrayColor)
-                .cornerRadius(20)
-            
-            TextField("휴대폰 번호를 입력하세요", text: $phoneNumber)
-                .textInputAutocapitalization(.never) // 첫글자 대문자 비활성화
-                .disableAutocorrection(true) // 자동수정 비활성화
-                .padding(10)
-                .padding(.leading, 5)
-                .background(Color.lightGrayColor)
-                .cornerRadius(20)
-            
+            VStack(alignment: .leading, spacing: 10) {
+                //MARK: 닉네임
+                Text("닉네임")
+                    .font(.pretendardBold14)
+                    .foregroundStyle(.primary)
+                    HStack {
+                        TextField("ex) 손흥민 (특수문자 불가)", text: $nickName)
+                            .textInputAutocapitalization(.never) // 첫글자 대문자 비활성화
+                            .disableAutocorrection(true) // 자동수정 비활성화
+                            .border(isNicknameValid ? Color.clear : Color.accentColor)
+                            .focused($focusField, equals: .nickName)
+                            .frame(width: .screenWidth*0.70, height: .screenHeight*0.05)
+                            .padding(.leading, 5)
+                            .background(Color.lightGrayColor)
+                            .cornerRadius(7)
+                            .onChange(of: nickName) { newValue in
+                                ischeckNickname()
+                                checkNickname = false
+                                nickName = newValue.trimmingCharacters(in: .whitespaces)
+                            }
+                        
+                        Spacer()
+                        //MARK: 중복확인
+                        if isHiddenCheckButton {
+                            Button {
+                                Task {
+                                    checkNickname = await authStore.doubleCheckNickname(nickname: nickName)
+                                    if checkNickname == false && nickName.count > 0 {
+                                        cautionNickname = "이미 사용 중인 닉네임"
+                                        checkNicknameColor = .red
+                                    } else {
+                                        cautionNickname = "사용 가능"
+                                        checkNicknameColor = .green
+                                    }
+                                }
+                            } label: {
+                                if checkNickname == false && true {
+                                    Text("중복확인")
+                                        .font(.pretendardBold18)
+                                        .foregroundStyle(nickName.count >= 0 ? .blue : .secondary)
+                                } else {
+                                    Text(cautionNickname)
+                                        .font(.pretendardBold18)
+                                        .foregroundStyle(checkNicknameColor)
+                                }
+                            }
+                            .padding(.trailing, 7)
+                        }
+                    } // HStack
+                if !isValidNickname(nickName) && nickName.count > 0 {
+                    Text(cautionNickname)
+                        .font(.pretendardMedium16)
+                        .foregroundStyle(checkNicknameColor)
+                }
+                //MARK: 전화번호
+                Text("전화번호")
+                    .font(.pretendardBold14)
+                    .foregroundStyle(.primary)
+                TextField("ex) 01098765432 (-)없이", text: $phoneNumber)
+                    .disableAutocorrection(true) // 자동수정 비활성화
+                    .frame(width: .screenWidth*0.70, height: .screenHeight*0.05)
+                    .padding(.leading, 5)
+                    .background(Color.lightGrayColor)
+                    .cornerRadius(7)
+                    .keyboardType(.numberPad)
+                    .onChange(of: phoneNumber) { newValue in
+                        if newValue.count > phoneNumberMaximumCount {
+                            phoneNumber = String(newValue.prefix(phoneNumberMaximumCount))
+                        }
+                    }
+            } // leading VStack
             Button {
                 userStore.user.nickname = nickName
                 userStore.user.phoneNumber = phoneNumber
                 
-                userStore.updateUser(user: userStore.user)
-            } label: {
-                Text("정보입력 완료하기")
-                    .foregroundStyle(.primary)
-            }
+                    userStore.updateUser(user: userStore.user)
+                } label: {
+                    Text("정보입력 완료하기")
+                }
             .buttonStyle(.borderedProminent)
+            .disabled(!checkNickname || phoneNumber.isEmpty)
             .padding()
             
             Button{
@@ -59,19 +138,37 @@ struct SignUpView: View {
             } label: {
                 HStack {
                     Text("로그아웃")
-                    Spacer()
                     Image(systemName: "chevron.right")
                 }
             }
-            
             Spacer()
-
+        } // 가장 큰 VStack
+        .padding(.horizontal, 12)
+    } // body
+    func ischeckNickname() {
+        if isValidNickname(nickName) {
+            cautionNickname = ""
+            isHiddenCheckButton = true
+            checkNicknameColor = .red
         }
+        else if !isValidNickname(nickName) && nickName.count > 0 {
+            cautionNickname = "닉네임 형식이 맞지 않습니다."
+            isHiddenCheckButton = false
+            checkNicknameColor = .red
+        }
+    }
+    func isValidNickname(_ nickName: String) -> Bool {
+        let nicknameExpression = "^[a-zA-Z0-9가-힣]+$"
+
+        let nickNamePredicate = NSPredicate(format:"SELF MATCHES %@", nicknameExpression)
+        return nickNamePredicate.evaluate(with: nickName)
     }
 }
 
 struct SignUpView_Previews: PreviewProvider {
     static var previews: some View {
         SignUpView()
+            .environmentObject(UserStore())
+            .environmentObject(AuthStore())
     }
 }
