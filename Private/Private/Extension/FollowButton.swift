168de3ca81bb7654ffe90f538e432317122ee003
Time5607 <115935304+Time5607@users.noreply.github.com>
2023-10-12 17:57:15 +0900

    [add] environmentObject(FollowStore()
    FollowStore.followCheck를 위한 변경

--- Private/Private/Extension/FollowButton.swift
+++ Private/Private/Extension/FollowButton.swift
@@ -9,29 +9,27 @@ import SwiftUI
 
 struct FollowButton: View {
     @EnvironmentObject var userStore: UserStore
-    @ObservedObject  var followStore = FollowStore()
+    @EnvironmentObject  var followStore: FollowStore
 
     var user:User
     @Binding var followingCount: Int
     @Binding var followersCount: Int
-    @Binding var followCheck: Bool
 
     @State private var backgroundColor = Color.white
 
-    init(user: User, followingCount: Binding<Int>, followersCount: Binding<Int>, followCheck: Binding<Bool>) {
+    init(user: User, followingCount: Binding<Int>, followersCount: Binding<Int>) {
         self.user = user
-        self._followCheck = followCheck
         self._followersCount = followersCount
         self._followingCount = followingCount
         
         // Initialize the background color state property based on the value of the followCheck property.
-        if followCheck.wrappedValue {
+        if followStore.followCheck {
             backgroundColor = Color("AccentColor")
         }
     }
 
     func follow(){
-        if !$followCheck.wrappedValue {
+        if !followStore.followCheck {
             followStore.follow(userId: user.nickname, myNickName: userStore.user.nickname, OtherEmail: user.email, followingCount: {
                 (followingCount) in
                 self.followingCount = followingCount
@@ -42,7 +40,7 @@ struct FollowButton: View {
 
             // Update the background color state property to indicate that the user is following.
             backgroundColor = Color("AccentColor")
-            self.followCheck = true
+            followStore.followCheck = true
         } else {
             followStore.unfollow(userId: user.nickname, myNickName: userStore.user.nickname,  userEmail: user.email, followingCount: {
                 (followingCount) in
@@ -54,7 +52,7 @@ struct FollowButton: View {
 
             // Update the background color state property to indicate that the user is not following.
             backgroundColor = Color.white
-            self.followCheck = false
+            followStore.followCheck = false
         }
     }
 
@@ -68,7 +66,7 @@ struct FollowButton: View {
 
     var body: some View {
         Button(action: follow) {
-            Text(($followCheck.wrappedValue) ? "팔로잉" : "팔로우")
+            Text((followStore.followCheck) ? "팔로잉" : "팔로우")
         }
                 .font(.pretendardBold18)
                 .frame(width: .screenWidth * 0.2, height: 12)
