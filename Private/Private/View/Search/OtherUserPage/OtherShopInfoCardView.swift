059d6865953a157706cb38f1c68aeab4d56c2820
jin_hyoung <37445660+mhn03148@users.noreply.github.com>
2023-10-17 15:47:21 +0900

    [Fix]otherpageview에서 장소 저장 취소 버튼이 눌리는 현상이 있어서 새로운 ShopInfoCardView 생성

---
+++ Private/Private/View/Search/OtherUserPage/OtherShopInfoCardView.swift
@@ -0,0 +1,18 @@
+//
+//  OtherShopInfoCardView.swift
+//  Private
+//
+//  Created by 주진형 on 10/17/23.
+//
+
+import SwiftUI
+
+struct OtherShopInfoCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OtherShopInfoCardView()
+}
